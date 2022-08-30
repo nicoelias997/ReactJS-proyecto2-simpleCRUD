01d60974e53d5abe171b0da5c1005241eb4ed26f
nicoelias997 <nicoelias997@gmail.com>
2022-08-30 19:48:00 +0200

    Agrego la estructura simple para nuestra lista de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,29 @@ import React from "react";
 
 function App() {
   return (
-    <div className="container mt-2">
+    <div className="container mt-5">
   <h1 className="text-center">CRUD APP</h1>
+  <hr />
+  <div className="row">
+    <div className="col-8">
+      <h4 className="text-center">
+      Lista de Tareas
+      </h4>
+      <ul className="list-group">
+        <li className="list-group-item">
+          <span className="lead">Nombre de la tarea</span>
+          <button className="btn btn-danger btn-sm float-end mx-2">Eliminar</button>
+          <button className="btn btn-warning btn-sm float-end">Editar</button>
+
+        </li>
+      </ul>
+    </div>
+    <div className="col-4">
+      <h4 className="text-center">
+      Formulario
+      </h4>
+    </div>
+  </div>
  
 </div>
   );

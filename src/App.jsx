cc59f5cf373c883119546805dff7059d66976901
nicoelias997 <nicoelias997@gmail.com>
2022-08-30 19:37:38 +0200

    Limpio el codigo, agrego el cdn de bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container mt-2">
+  <h1 className="text-center">CRUD APP</h1>
+ 
+</div>
+  );
+}
+
+export default App;
